f56a1b2424200372359d4d2a552266924deed239
AbidHasanRafi <abid.h.ece@gmail.com>
2024-02-18 02:53:00 +0600

    updated header section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "bus-primary": "rgb(29, 209, 0)",
+      },
+    },
+  },
+  plugins: [],
+};
