effaa0bb2fb44086d4e1c2b6acb9cb6b1b831e77
AbidHasanRafi <abid.h.ece@gmail.com>
2024-02-18 17:25:28 +0600

    added main section

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("I'm Live");
+const selectSeats = document.querySelectorAll(".seat");
+let count = 0;
+for (const selectSeat of selectSeats) {
+  count = count + 1;
+  selectSeat.addEventListener("click", function () {
+    const addPrice = document.createElement("p");
+    addPrice.innerText = 550;
+
+    const classType = document.createElement("p");
+    classType.innerText = "Economic";
+
+    const seatName = selectSeat.innerText;
+    const seatNameElement = document.createElement("p");
+    seatNameElement.textContent = seatName;
+
+    const cartName = document.getElementById("cart-name");
+    const cartPrice = document.getElementById("cart-price");
+    const cartType = document.getElementById("cart-class");
+    cartType.appendChild(classType);
+    cartPrice.appendChild(addPrice);
+    cartName.appendChild(seatNameElement);
+  });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        "bus-primary": "rgb(29, 209, 0)",
-      },
+      colors: {},
     },
   },
   plugins: [],
